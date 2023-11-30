e4bcd376173a01106b1d4289fc240ff7f3c603df
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2023-11-30 10:09:02 -0300

    Create transfer_data_BQ_to_bucket.sh

---
+++ transfer_data_BQ_to_bucket.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+r=southamerica-east1
+p=cerc2-gestop-prd-01
+
+
+echo "set project $p"
+gcloud config set project $p
+
+
+echo "set region $r"
+gcloud config set compute/region $r
+
+
+tables=("alert_contract_effect_q4"
+"")
+
+
+for item in "${tables[@]}"; do
+	echo "transfer table $item from bq to bucket"
+    bq extract \
+	  --destination_format=PARQUET \
+      --compression SNAPPY \
+      "$p:monitoring.$item" "gs://backup_gestop_monitoring/$item/*.parquet"
+done
