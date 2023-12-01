7c6cda3a97c302c69dde9ac174ed75cea3b32e96
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2023-12-01 11:32:37 -0300

    Create pub_sub_publisher.py

---
+++ pub_sub_publisher.py
@@ -0,0 +1,15 @@
+from google.cloud import pubsub_v1
+
+
+def message_contains_file_name(self):
+        project_id = "cerc2-gestop-stg"
+        topic_name = "file-name-conc-received"
+        message_data = self.zip_var
+
+        self.conn_pub_sub()
+        topic_path = self.publisher.topic_path(project_id, topic_name)
+
+        message = message_data.encode("utf-8")
+
+        future = self.publisher.publish(topic_path, data=message)
+        print(f"Published message ID: {future.result()}")
