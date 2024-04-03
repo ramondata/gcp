1698946212bb7d11517bc8ceb0df30cb1be525b8
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2024-04-03 16:48:22 -0300

    Update table_columns.sql

--- BigQuery/table_columns.sql
+++ BigQuery/table_columns.sql
@@ -2,3 +2,10 @@
 
 SELECT * FROM 
  `cerc2-gestop-stg.interop.INFORMATION_SCHEMA.COLUMNS`;
+
+-- Are there any partitioned or clustered columns?
+
+SELECT * FROM 
+ `cerc2-gestop-stg.interop.INFORMATION_SCHEMA.COLUMNS`
+WHERE 
+  is_partitioning_column = 'YES' OR clustering_ordinal_position IS NOT NULL;
