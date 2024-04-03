0a15125f20f9e84782c01ac0a9b17340a622ebe1
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2024-04-03 16:55:53 -0300

    Update dataset_information.sql

--- BigQuery/dataset_information.sql
+++ BigQuery/dataset_information.sql
@@ -1,10 +1,10 @@
 -- information about dataset
 
 SELECT *
-FROM `cerc2-gestop-prd-01.region-southamerica-east1`.INFORMATION_SCHEMA.SCHEMATA
+FROM `<project-id>.region-southamerica-east1`.INFORMATION_SCHEMA.SCHEMATA
 where schema_name = '<dataset-id>'
 
 
 SELECT *
-FROM `cerc2-gestop-prd-01.region-southamerica-east1`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
+FROM `<project-id>.region-southamerica-east1`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
 where schema_name = '<dataset-id>'
