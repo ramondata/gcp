e2226c6767841c181b6fb21d0297a4f0d708608b
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2024-04-03 14:55:10 -0300

    Create table_info_size_rownumber.sql

---
+++ BigQuery/table_info_size_rownumber.sql
@@ -0,0 +1,20 @@
+-- Get information about size and row number
+
+SELECT 
+  dataset_id,
+  table_id,
+  -- Convert bytes to GB.
+  ROUND(size_bytes/pow(10,9),2) as size_gb,
+  -- Convert UNIX EPOCH to a timestamp.
+  TIMESTAMP_MILLIS(creation_time) AS creation_time,
+  TIMESTAMP_MILLIS(last_modified_time) as last_modified_time,
+  row_count,
+  CASE 
+    WHEN type = 1 THEN 'table'
+    WHEN type = 2 THEN 'view'
+    WHEN type = 3 THEN 'external source'
+  ELSE NULL
+  END AS type
+FROM
+  `<project><dataset>.__TABLES__`
+ORDER BY size_gb DESC;
